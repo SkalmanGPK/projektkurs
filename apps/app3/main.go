274e495b00b01c9f5c0b1c59d98971393cdc7ab8
Robin Lust <pineappel92@gmail.com>
2026-04-20 14:05:07 +0200

    fixade kommentarer för app3 main.go fil

--- apps/app3/main.go
+++ apps/app3/main.go
@@ -1,29 +1,30 @@
-package main
+package main // defining that this is a runnable application
 
 import (
-	"database/sql"
-	"enconding/json"
-	"fmt"
-	"log"
-	"net/http"
+	"database/sql" // package to speak with SQL databases
+	"encoding/json" // used for JSON
+	"fmt" //to write text in the terminal
+	"log" // Logging issues
+	"net/http" // To create webservers
+	- "github.com/go-sql-driver/mysql" // To let go understand how to speak with mysql
 )
 
-type SensorData struct {
-	Timestamp string 'json:"timestamp"'
-	MachineID string 'json:"machine_id"'
-	Temperature int 'json:"temperature"'
-	Pressure int 'json:"pressure"'
+type SensorData struct { // mapping JSON-fields to go-variables
+	Timestamp string `json:"timestamp"`
+	MachineID string `json:"machine_id"`
+	Temperature int `json:"temperature"`
+	Pressure int `json:"pressure"`
 }
 
-var db *sql.DB
+var db *sql.DB // A global variable that holds the database connection open to the entire app
 
-func ingestHandler(w http.ResponseWriter, r *http.Request) {
-	
+func ingestHandler(w http.ResponseWriter, r *http.Request) { // handler function, runs everytime a call is made to the server.
+	defer r.Body.Close() // Closes the stream when func is done.
 	var data SensorData
 
-	err := json.NewDecoder(r.Body).Decode(&data)
+	err := json.NewDecoder(r.Body).Decode(&data) // Reading JSON data from the call and saves it in the "data-variable"
 	if err != nil {
-		http.Error(w, "bad request", 400)
+		http.Error(w, "bad request", 400) // if JSON is broken, answer with code 400.
 		return
 	}
 
